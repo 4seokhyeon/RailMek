e5e46262057fb0a58323eb747ce0548f1b5e57cb
4seokhyeon <tktjrgus@naver.com>
2023-12-31 19:46:19 +0900

    혼잡도 패키지 분리 및 프래그먼트 뷰바인딩 설정

--- app/src/main/java/com/subway/railmek/congestion/CongestionFragment.kt
+++ app/src/main/java/com/subway/railmek/congestion/CongestionFragment.kt
@@ -6,17 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.subway.railmek.R
+import com.subway.railmek.databinding.FragmentCongestionBinding
 
 class CongestionFragment : Fragment() {
 
+    private var _binding: FragmentCongestionBinding? = null
+    private val binding get() =_binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_congestion, container, false)
+        _binding = FragmentCongestionBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 
