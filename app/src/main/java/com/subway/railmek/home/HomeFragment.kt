4f26def080bf3f5329d065ce488a06284caa2ca5
4seokhyeon <tktjrgus@naver.com>
2024-01-07 13:40:00 +0900

    홈프래그먼트 viewbinding 적용

--- app/src/main/java/com/subway/railmek/home/HomeFragment.kt
+++ app/src/main/java/com/subway/railmek/home/HomeFragment.kt
@@ -6,15 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.subway.railmek.R
+import com.subway.railmek.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    private var _binding:FragmentHomeBinding?=null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding= FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 
