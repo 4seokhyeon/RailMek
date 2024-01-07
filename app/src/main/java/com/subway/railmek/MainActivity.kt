f6deaf6e3a36a81090fd9d8876e06f2c8af46d01
4seokhyeon <tktjrgus@naver.com>
2024-01-07 13:40:25 +0900

    메인엑티비티 바텀 하단바 그래프 적용

--- app/src/main/java/com/subway/railmek/MainActivity.kt
+++ app/src/main/java/com/subway/railmek/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.subway.railmek
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.subway.railmek.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
+        //Component 네비게이션 바텀 하단바 적용
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.my_nav_host) as NavHostFragment?
+        val navController = navHostFragment!!.navController
+        setupWithNavController(_binding!!.myBottomNav, navController)
         setContentView(binding.root)
     }
 }
