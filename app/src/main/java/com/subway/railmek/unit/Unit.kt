6929fd3f07812395f0fc6703eb450d4aecbcf546
4seokhyeon <tktjrgus@naver.com>
2024-01-16 14:39:11 +0900

    인증키 및 BASE_URL 선언 인증키 싱글톤 선언

---
+++ app/src/main/java/com/subway/railmek/unit/Unit.kt
@@ -0,0 +1,8 @@
+package com.subway.railmek.unit
+
+import com.subway.railmek.BuildConfig
+
+object Unit {
+    const val API = BuildConfig.SubWay_Api_Key
+    const val BASE_URL = "http://swopenapi.seoul.go.kr/"
+}
