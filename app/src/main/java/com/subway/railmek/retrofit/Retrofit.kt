96ba71baec0df7ffc2aff10ffddb67a48f56406f
4seokhyeon <tktjrgus@naver.com>
2024-01-16 14:38:27 +0900

    레트로핏 통신 클래스 및 기본 뼈대 구현

--- app/src/main/java/com/subway/railmek/retrofit/Retrofit.kt
+++ app/src/main/java/com/subway/railmek/retrofit/Retrofit.kt
@@ -1,4 +1,30 @@
 package com.subway.railmek.retrofit
 
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
+import com.subway.railmek.unit.Unit.API
+import com.subway.railmek.unit.Unit.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object Retrofit {
+    val api: RetrofitInterface get() = instance.create(RetrofitInterface::class.java)
+
+    private val instance: Retrofit
+        get() {
+            val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
+                val request: Request = chain.request()
+                    .newBuilder()
+                    .addHeader("Authorization", "$API")
+                    .build()
+                chain.proceed(request)
+            }.build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
+                .build()
+        }
 }
