b9139d27a038b3a03ba0a3a7b229920600a3406d
4seokhyeon <tktjrgus@naver.com>
2024-01-17 14:37:59 +0900

    subway 데이터 클래스 생성

---
+++ app/src/main/java/com/subway/railmek/data/SubwayInfo.kt
@@ -0,0 +1,12 @@
+package com.subway.railmek.data
+
+data class SubwayInfo(
+    val mreaWideCd: Int,
+    val routCd: Int,
+    val routNm: String,
+    val stinConsOrdr: String,
+    val railOprIsttCd: String,
+    val lnCd: String,
+    val stinCd: String,
+    val stinNm: String
+)
