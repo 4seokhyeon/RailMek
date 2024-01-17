5a8de808e41e0c19e6997a458a74a9ee64815580
4seokhyeon <tktjrgus@naver.com>
2024-01-17 14:38:19 +0900

    레트로핏 인터페이스 설계

--- app/src/main/java/com/subway/railmek/retrofit/RetrofitInterface.kt
+++ app/src/main/java/com/subway/railmek/retrofit/RetrofitInterface.kt
@@ -1,4 +1,17 @@
 package com.subway.railmek.retrofit
 
+import com.subway.railmek.data.SubwayInfo
+import com.subway.railmek.unit.Unit.API
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface RetrofitInterface {
+
+    @GET("subwayRouteInfo")
+    suspend fun getSubwayInfo(
+        @Query("serviceKey") serviceKey: String = API,
+        @Query("format") format: String ="json",
+        @Query("lnCd") lnCd: String ="1",
+        @Query("mreaWideCd") mreaWideCd: String ="01"
+    ): SubwayInfo
 }
