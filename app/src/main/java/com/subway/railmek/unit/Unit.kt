e5c187087f4a882d528b831667882dbeba1363de
4seokhyeon <tktjrgus@naver.com>
2024-01-17 15:33:36 +0900

    실시간 도착정보 인증키 및 url 생성

--- app/src/main/java/com/subway/railmek/unit/Unit.kt
+++ app/src/main/java/com/subway/railmek/unit/Unit.kt
@@ -4,5 +4,8 @@ import com.subway.railmek.BuildConfig
 
 object Unit {
     const val API = BuildConfig.SubWay_Api_Key
+    const val API2 = BuildConfig.SubWay_realtime_Key
+
+    const val BASE_URL_REALTIME = "https://data.seoul.go.kr/dataList/OA-12764/A/1/datasetView.do"
     const val BASE_URL = "https://openapi.kric.go.kr/openapi/trainUseInfo/subwayRouteInfo"
 }
