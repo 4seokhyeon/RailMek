d5edd0ea126fb905107c45115df770be146d4203
4seokhyeon <tktjrgus@naver.com>
2024-01-17 17:40:41 +0900

    interface에 인증키와 주석 추가

--- app/src/main/java/com/subway/railmek/data/SubwayArrivalInfo.kt
+++ app/src/main/java/com/subway/railmek/data/SubwayArrivalInfo.kt
@@ -20,5 +20,5 @@ data class SubwayArrivalInfo(
     val recptnDt: String,
     val arvlMsg2: String,
     val arvlMsg3: String,
-    val arvlCd: String
+    val arvlCd: String  //도착 코드 역을 검색 or 마커 클릭하면 정보가 모두 뜨게 해야할거같음
 )
--- app/src/main/java/com/subway/railmek/retrofit/RealTimeInterface.kt
+++ app/src/main/java/com/subway/railmek/retrofit/RealTimeInterface.kt
@@ -1,6 +1,7 @@
 package com.subway.railmek.retrofit
 
 import com.subway.railmek.data.SubwayArrivalInfo
+import com.subway.railmek.unit.Unit.API2
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,9 +9,9 @@ interface RealTimeInterface {
 
     @GET("realtimeStationArrival")
     suspend fun getRealtimeInfo(
-        @Query("KEY") key: String,
-        @Query("TYPE") type: String,
-        @Query("SERVICE") service: String,
+        @Query("KEY") key: String = API2,
+        @Query("TYPE") type: String = "json",
+        @Query("SERVICE") service: String = "realtimeStationArrival",
         @Query("START_INDEX") startIndex: Int,
         @Query("END_INDEX") endIndex: Int,
         @Query("statnNm") statnNm: String
